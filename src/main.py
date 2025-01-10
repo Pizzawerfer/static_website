6ff426092dee2b14319654bcea3b21a4db5783f0
Pizzawerfer <pizzawerfer@hotmail.de>
2025-01-10 15:47:12 +0100

    Testing added

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
-from textnode import *
+from textnode import TextNode, TextType
 
 def main():
     Node = TextNode("This is a text node", "bold", "https://www.boot.dev)")
     print(Node)
 
 
-    
+
 main()
