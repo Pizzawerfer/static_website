39551a6c14bce5147a3e4e466900a0b29059ad20
Pizzawerfer <pizzawerfer@hotmail.de>
2025-01-10 19:27:24 +0100

    split delimiter added

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node
+from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -47,7 +47,37 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
         self.assertEqual(html_node.tag, "b")
         self.assertEqual(html_node.value, "This is bold")
 
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_split_nodes_delimiter(self):
+        node = TextNode("*This `is` italic*", TextType.ITALIC)
+        nodes = split_nodes_delimiter(node, "`", TextType.ITALIC)
+        self.assertEqual(nodes[0].text, "*This ")
+        self.assertEqual(nodes[0].text_type, TextType.ITALIC)
+        self.assertEqual(nodes[1].text, "is")
+        self.assertEqual(nodes[1].text_type, TextType.CODE)
+        self.assertEqual(nodes[2].text, " italic*")
+        self.assertEqual(nodes[2].text_type, TextType.ITALIC)
 
+    def test_split_nodes_delimiter2(self):
+        node = TextNode("This `is` italic", TextType.TEXT)
+        nodes = split_nodes_delimiter(node, "`", TextType.TEXT)
+        self.assertEqual(nodes[0].text, "This ")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text, "is")
+        self.assertEqual(nodes[1].text_type, TextType.CODE)
+        self.assertEqual(nodes[2].text, " italic")
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
         
+    def test_split_nodes_delimiter3(self):
+        node = TextNode("This **is** italic", TextType.TEXT)
+        nodes = split_nodes_delimiter(node, "**", TextType.TEXT)
+        self.assertEqual(nodes[0].text, "This ")
+        self.assertEqual(nodes[0].text_type, TextType.TEXT)
+        self.assertEqual(nodes[1].text, "is")
+        self.assertEqual(nodes[1].text_type, TextType.BOLD)
+        self.assertEqual(nodes[2].text, " italic")
+        self.assertEqual(nodes[2].text_type, TextType.TEXT)
+
+
 if __name__ == "__main__":
     unittest.main()
